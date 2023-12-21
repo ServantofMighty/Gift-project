ceeb3c3b8f2a440f0360abc232f685a803dde965
unknown <anifowosetoyib@gmail.com>
2023-12-21 18:11:49 +0100

    My merkle Tree

--- client/index.js
+++ client/index.js
@@ -1,14 +1,31 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const serverUrl = "http://localhost:1225";
+const readline = require("readline");
 
-const serverUrl = 'http://localhost:1225';
+/*const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+rl.question("What is your name? ", (name) => {
+  console.log("You entered: " + name);
+  rl.close();
+});*/
+const name = "Anifowose Temitope";
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  // TODO: how do we prove to the server we're on the nice list?
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  console.log(root);
+  const index = niceList.findIndex((n) => n === name);
+  console.log(index);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,18 +8,18 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "e326e9a8384a5cfad19a863aa6372f081b63ad52fe40383bdac910146b434aa8";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
